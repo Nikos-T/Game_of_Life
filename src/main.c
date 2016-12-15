759d103c01d46a5c19ae165395187d39265b4e56
Nikos-T <nikos.terzo@gmail.com>
2016-12-15 17:33:26 +0200

    Update main.c
    The interpretation of the assignment is 40000x40000 cells per task
    So:
    -We need 4 arguments
    Also:
    -Created new world nodes.

--- src/main.c
+++ src/main.c
@@ -28,11 +28,10 @@ int name_to_color(char *processor_name) {
 int main (int argc, char *argv[]) {
   int   *board, *newboard, i;
 
-  if (argc != 6) { // Check if the command line arguments are correct 
+  if (argc != 5) { // Check if the command line arguments are correct 
     printf("Usage: %s M N thres disp\n"
            "where\n"
-           "  M     : size of table (M x N)\n"
-           "  N     : size of table (M x N)\n"
+           "  N     : size of table per process (N x N)\n"
            "  thres : propability of alive cell\n"
            "  t     : number of generations\n"
            "  disp  : {1: display output, 0: hide output}\n"
@@ -49,29 +48,38 @@ int main (int argc, char *argv[]) {
   int len;
   MPI_Get_processor_name(pname, &len);
   int node_key = name_to_color(pname);
-  MPI_Comm node_world;
-  MPI_Comm_split(MPI_COMM_WORLD, node_key, 0, &node_world);
-  int node_nthreads, TID;
-  MPI_Comm_size(node_world, &node_nthreads);
-  omp_set_num_threads(node_nthreads); //threads per node
-  MPI_Comm_rank(node_world, &TID);
+  MPI_Comm my_world;  //inside-processor communicator
+  MPI_Comm_split(MPI_COMM_WORLD, node_key, 0, &my_world);
+  int node_nthreads, TID; //TID is thread ID
+  MPI_Comm_size(my_world, &node_nthreads);
+  omp_set_num_threads(node_nthreads); //set threads per node
+  MPI_Comm_rank(my_world, &TID);
   if (TID!=0) {
     MPI_Finalize();
     return(0);
   }
-  int world_size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &TID);
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  int world_size; //don't need
+  MPI_Comm_rank(MPI_COMM_WORLD, &TID);  //TID is task ID from MPI_COMM_WORLD
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size); //don't need
   
   //debug
   printf("Hello from %i of %i\n", TID, world_size);
-  /* temporary
+  
+  MPI_Comm_split(MPI_COMM_WORLD, 0, TID, &my_world);  //here my_world is outside-processor communicator
+  int nNodes;
+  MPI_Comm_size(my_world, &nNodes);
+  MPI_Comm_rand(my_world, &TID);  //TID is node ID
+  
+  //debug
+  printf("Hello from node %i of %i", TID, nNodes);
+  
+  
+  /*temporary
   // Input command line arguments
-  int M = atoi(argv[1]);
-  int N = atoi(argv[2]);        // Array size
-  double thres = atof(argv[3]); // Propability of life cell
-  int t = atoi(argv[4]);        // Number of generations 
-  int disp = atoi(argv[5]);     // Display output?
+  int N = atoi(argv[1]);        // Horizontal size
+  double thres = atof(argv[2]); // Propability of life cell
+  int t = atoi(argv[3]);        // Number of generations 
+  int disp = atoi(argv[4]);     // Display output?
   printf("Size %dx%d with propability: %0.1f%%\n", M, N, thres*100);
 
   board = NULL;

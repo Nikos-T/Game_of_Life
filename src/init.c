dddb3578fb446f60ebffbdd8f68ffa55eb568f4e
Nikos-T <nikos.terzo@gmail.com>
2016-12-21 21:07:21 +0200

    gener random table

--- src/init.c
+++ src/init.c
@@ -26,6 +26,7 @@ void generate_table (int *board, int N, float threshold, int nodeID) {
   srand(time(NULL)+nodeID);
   #pragma omp parallel for
   for (i=0; i<N; i++) {
+    if (i=0) printf("\nNum threads = %i\n", omp_get_num_threads());
     for (j=0; j<N; j++) {
       Board(i,j) = ( (float)rand() / (float)RAND_MAX ) < threshold;
     }

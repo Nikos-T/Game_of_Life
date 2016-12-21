08f0f3948ec2b7390355e084f149817801925012
Nikos-T <nikos.terzo@gmail.com>
2016-12-21 15:37:15 +0200

    play2-play

--- src/main.c
+++ src/main.c
@@ -360,7 +360,7 @@ int main (int argc, char *argv[]) {
       }
       transfer_boundaries(board, N, boundaries);
       MPI_Barrier(my_world);
-      play2(board, newboard, N);
+      play2(board, newboard, N, boundaries, 4);
       MPI_Barrier(my_world);
     }
   }

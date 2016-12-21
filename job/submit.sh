812ef201c9b4065400071f5dd2eed17f1d9039dd
Nikos-T <nikos.terzo@gmail.com>
2016-12-22 00:37:53 +0200

    serial generate

--- job/submit.sh
+++ job/submit.sh
@@ -10,7 +10,7 @@ cd $PBS_O_WORKDIR
 export OMP_NUM_THREADS=8
 echo "=====test Run starts now ======= `date` "
 
-mpiexec -np $PBS_NP ./../bin/game-of-life 40000 0.05 3 0 &> $PBS_JOBNAME.log
+mpiexec -np $PBS_NP ./../bin/game-of-life 40000 0.05 3 1 &> $PBS_JOBNAME.log
 
 
 echo "====test Run ends now ======= `date` "

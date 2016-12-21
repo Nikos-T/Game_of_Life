185afcfc92d13788d36ab076cbf9db71b2663c3b
Nikos-T <nikos.terzo@gmail.com>
2016-12-21 22:06:02 +0200

    gener random table

--- src/init.c
+++ src/init.c
@@ -11,8 +11,8 @@ void initialize_board (int *board, int N) {
   
   #pragma omp parallel for collapse(2)
   for (int i=0; i<N; i++) {
-    printf("omp num threads = %i\n", omp_get_num_threads());
     for (int j=0; j<N; j++) {
+      if (i==0 && j==0) printf("omp num threads = %i", omp_get_num_threads());
       Board(i,j) = 0;
     }
   }

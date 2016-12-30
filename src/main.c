e909c52d07f93979160f0357a67b1149d65935aa
Nikos-T <nikos.terzo@gmail.com>
2016-12-30 22:06:02 +0200

    MPI init

--- src/main.c
+++ src/main.c
@@ -219,8 +219,9 @@ int main (int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &nodeID);  //TID is task ID from MPI_COMM_WORLD
   MPI_Comm_size(MPI_COMM_WORLD, &nNodes);
   MPI_Get_processor_name(pname, &len);
+  #pragma omp parallel
   len = omp_get_num_threads();
-  printf("I am node %s.\nMy rank is %i.\nI have %i threads.", pname, nodeID, len);
+  printf("I am node %s.\nMy rank is %i.\nI have %i threads.\n", pname, nodeID, len);
   if (nNodes!=1 && nNodes!=2 && nNodes!=4) {  //check nodes=1,2 or 4
     printf("nNodes = %i\nThis many nodes not supported\n", nNodes);
     MPI_Finalize();

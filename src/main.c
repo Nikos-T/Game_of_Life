b84f345886b24b522e150b223069cc5be5642fef
Nikos-T <nikos.terzo@gmail.com>
2017-01-01 22:32:07 +0200

    final?

--- src/main.c
+++ src/main.c
@@ -109,14 +109,14 @@ void transfer_boundaries(int *board, int N, int *boundaries) {
 void display_table2(int *board, int N) {
   for (int j=0; j<N; j++) {
     MPI_Barrier(MPI_COMM_WORLD);
-    //usleep(10000);
+    usleep(5000);
     if (nodeID==0) {
       for (int i=0; i<N; i++) {
         printf ("%c", Board(i,j) ? 'x' : ' ');
       }
     }
     MPI_Barrier(MPI_COMM_WORLD);
-    //usleep(10000);
+    usleep(5000);
     if (nodeID==1) {
       for (int i=0; i<N; i++) {
         printf ("%c", Board(i,j) ? 'x' : ' ');
@@ -130,14 +130,14 @@ void display_table2(int *board, int N) {
   }
   for (int j=0; j<N; j++) {
     MPI_Barrier(MPI_COMM_WORLD);
-    //usleep(10000);
+    usleep(5000);
     if (nodeID==2) {
       for (int i=0; i<N; i++) {
         printf ("%c", Board(i,j) ? 'x' : ' ');
       }
     }
     MPI_Barrier(MPI_COMM_WORLD);
-    //usleep(10000);
+    usleep(5000);
     if (nodeID==3) {
       for (int i=0; i<N; i++) {
         printf ("%c", Board(i,j) ? 'x' : ' ');

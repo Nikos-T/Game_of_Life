1498988513b4a0efde6f75bd5360e10282fb44e4
Nikos-T <nikos.terzo@gmail.com>
2017-01-01 22:22:24 +0200

    final?

--- job/check_glider.sh
+++ job/check_glider.sh
@@ -11,6 +11,6 @@ export OMP_NUM_THREADS=8
 
 echo "=====test Run starts now ======= `date` "
 
-mpiexec -np $PBS_NUM_NODES -ppn 1 ./../bin/game-of-life 32 0.2 500 1 1 &> $PBS_JOBNAME.log
+mpiexec -np $PBS_NUM_NODES -ppn 1 ./../bin/game-of-life 8 0.2 500 1 1 &> $PBS_JOBNAME.log
 
 echo "====test Run ends now ======= `date` "

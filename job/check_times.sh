4a3ca23b5e26af46c5b9a76be8e2d092ef39640f
Nikos-T <nikos.terzo@gmail.com>
2017-01-04 18:24:55 +0200

    serial3

--- job/check_times.sh
+++ job/check_times.sh
@@ -5,12 +5,11 @@
 #PBS -j oe
 #PBS -l nodes=1:ppn=1
 
-module load mpi/mpich3-x86_64
+
 cd $PBS_O_WORKDIR
-export OMP_NUM_THREADS=1
 
 echo "=====test Run starts now ======= `date` "
 
-mpiexec -np $PBS_NUM_NODES -ppn 1 ./../bin/game-of-life 40000 0.5 3 0 0&> $PBS_JOBNAME.log
+./../bin/game-of-life 40000 0.5 3 0 0&> $PBS_JOBNAME.log
 
 echo "====test Run ends now ======= `date` "
